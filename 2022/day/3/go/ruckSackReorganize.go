c5b6ead4d3a9acc301944f37d04c33f6fd6f138c
Isaac Vicente <contatoisaacvicente@gmail.com>
2023-09-02 13:23:07 -0300

    Resolve part 2 and finishes day 3

--- 2022/day/3/go/ruckSackReorganize.go
+++ 2022/day/3/go/ruckSackReorganize.go
@@ -9,6 +9,11 @@ import (
 )
 
 func main() {
+	part1()
+	part2()
+}
+
+func part1() {
 	file, err := os.Open("../input.txt")
 	if err != nil {
 		log.Fatal(err)
@@ -30,17 +35,70 @@ func main() {
 			char := search(right, left[i])
 			if char != '0' {
 				repeated = append(repeated, char)
-				if unicode.IsUpper(char) {
-					sum += int(char) - 38
-				} else {
-					sum += int(char) - 96
-				}
+				sum += priority(char)
 				break
 			}
 		}
 	}
 
-	fmt.Println("Sum of priorities:", sum)
+	fmt.Println("Part 1 - Sum of priorities:", sum)
+}
+
+func part2() {
+	file, err := os.Open("../input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var repeated []rune
+	var common []rune
+	var sum int
+	var lines []string
+	var first []rune
+	var second []rune
+	var third []rune
+
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	for i := 0; i < len(lines); i += 3 {
+		first = []rune(lines[i])
+		second = []rune(lines[i+1])
+		third = []rune(lines[i+2])
+
+		for i := 0; i < len(first); i++ {
+			char := search(second, first[i])
+			if char != '0' {
+				repeated = append(repeated, char)
+			}
+		}
+
+		for i := len(repeated) - 1; i >= 0; i-- {
+			char := search(third, repeated[i])
+			if char != '0' {
+				common = append(common, char)
+				break
+			}
+		}
+	}
+
+	for j := 0; j < len(common); j++ {
+		sum += priority(common[j])
+	}
+
+	fmt.Println("Part 2 - Sum of priorities:", sum)
+}
+
+func priority(r rune) int {
+	if unicode.IsUpper(r) {
+		return int(r) - 38
+	} else {
+		return int(r) - 96
+	}
 }
 
 func search(a []rune, k rune) rune {
